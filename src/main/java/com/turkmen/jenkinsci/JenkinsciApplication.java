52261f2323d4c3f5c4f62318ecf9cb768c53871a
deniz.turkmen <turkmen_deniz@hotmail.com>
2023-02-18 18:22:37 +0300

    first commit

---
+++ src/main/java/com/turkmen/jenkinsci/JenkinsciApplication.java
@@ -0,0 +1,13 @@
+package com.turkmen.jenkinsci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsciApplication.class, args);
+	}
+
+}
